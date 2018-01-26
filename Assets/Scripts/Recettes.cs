cc8762362a316830773a929abe120410d66e0273
KangNansi <evillz@hotmail.com>
2018-01-26 23:07:13 +0100

    recette++

--- Assets/Scripts/Recettes.cs
+++ Assets/Scripts/Recettes.cs
@@ -7,6 +7,38 @@ public class Recettes : ScriptableObject {
     [SerializeField]
     public List<Ingredients> ingredients = new List<Ingredients>();
 
+    public enum Type{
+        Ordered, Balec
+    };
 
+    Type type;
+
+    public bool ConsumeIngredient(Ingredients ingredient)
+    {
+        if(type == Type.Ordered)
+        {
+            if(ingredients.Count>0 && ingredients[0].name == ingredient.name)
+            {
+                ingredients.RemoveAt(0);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            foreach(Ingredients i in ingredients)
+            {
+                if(i.name == ingredient.name)
+                {
+                    ingredients.Remove(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 
 }
