30cde373265a745d8f18a10950a1d2f7fd9e2f2f
KangNansi <evillz@hotmail.com>
2018-01-27 13:58:26 +0100

    story management

---
+++ Assets/Scripts/Story.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Story : MonoBehaviour {
+
+    public List<Level> levels;
+    int next = 0;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void LaunchNext()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync("main");
+        op.completed += (o) =>
+        {
+            LevelManager.Manager.type = LevelManager.Type.Normal;
+            LevelManager.Manager.level = levels[next];
+            LevelManager.Manager.Init();
+            next++;
+        };
+    }
+}
