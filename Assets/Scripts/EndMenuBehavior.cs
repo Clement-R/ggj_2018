fab0800bd8480450cedd0ccdda400484155a0085
Clement-R <rondeau.clement@gmail.com>
2018-01-27 23:23:50 +0100

    WIP on end menu

---
+++ Assets/Scripts/EndMenuBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using pkm.EventManager;
+
+public class EndMenuBehavior : MonoBehaviour {
+
+	void Start ()
+    {
+        EventManager.StartListening("TogglePause", OnEnd);
+	}
+
+    void OnEnd(object obj)
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+
+        transform.GetChild(0).GetChild(1).GetComponent<Text>().text = LevelManager.Manager.Score.ToString();
+    }
+	
+	void Update ()
+    {
+		// TODO : Manager players inputs
+	}
+}
