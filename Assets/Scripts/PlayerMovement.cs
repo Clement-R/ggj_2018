8dd575512a9a59f549fa9f3db61396cb4f10b5aa
Clement-R <rondeau.clement@gmail.com>
2018-01-27 13:11:23 +0100

    Add player actions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    [Header("Ingredients")]
+    public Ingredients lightItUp;
+    public Ingredients Stire;
+    public Ingredients StireReverse;
+    public Ingredients Shake;
+
+    [Space(25)]
+
     public float cooldownMove = 0.5f;
     public GameObject otherPlayer;
     public Transform[] positions;
@@ -22,11 +30,25 @@ public class PlayerMovement : MonoBehaviour {
     private SpriteRenderer _sr;
     private int _playerId;
 
+    private float _timestampStireBlock = 0f;
+    private float _timestampShakeBlock = 0f;
+
+    private bool _isInShakeStance = false;
+    // U : Up / D : Down
+    private string _shakeCombination = "UDUDUD";
+    private string _actualShakeCombination = "";
+    private bool _detectShakeValue = true;
+
+    private bool _isInStireStance = false;
+    private string _stireCombination = "XYBA";
+    private string _stireReverseCombination = "XABY";
+    private string _actualStireCombination = "";
+
     private void Start()
     {
         _sr = GetComponent<SpriteRenderer>();
         _sign = Mathf.Sign(positions[1].position.x - positions[0].position.x);
-        _playerId = _sign == -1 ? 0 : 1;
+        _playerId = _sign == -1 ? 1 : 0;
 
         LevelManager.Manager.joueur1Ended += OnRecipeEnd;
     }
@@ -35,6 +57,8 @@ public class PlayerMovement : MonoBehaviour {
     {
         print(r.name);
     }
+
+    // TODO : Block player in shake or stire stance if actual recipe is ordered and next ingredient is a stire or shake
 
     void Update ()
     {
@@ -46,6 +70,10 @@ public class PlayerMovement : MonoBehaviour {
         {
             StartCoroutine(Move(-1 * (int)_sign));
         }
+        else
+        {
+            _canMove = true;
+        }
         
         if (Input.GetAxisRaw(axisHigh) > 0.5 && _canMoveHigh && _actualPositionIndex != 0)
         {
@@ -55,6 +83,10 @@ public class PlayerMovement : MonoBehaviour {
         {
             StartCoroutine(MoveHigh(1));
         }
+        else
+        {
+            _canMoveHigh = true;
+        }
 
         if(Input.GetButtonDown(validationKey) && _actualPositionIndex != 0)
         {
@@ -62,16 +94,124 @@ public class PlayerMovement : MonoBehaviour {
             AddIngredient();
         }
 
-        // TODO : detect stire movement, and add null ingredient if it's not done well
-        if (Input.GetButtonDown("A_1") && _actualPositionIndex == 0)
+        if(LevelManager.Manager.GetNextIngredient(_playerId) != null)
         {
-            // AddIngredient();
+            string ingredientName = LevelManager.Manager.GetNextIngredient(_playerId).name;
+            
+            if ((ingredientName == "stire" || ingredientName == "reverse_stire") && _actualPositionIndex == 0 && !_isInStireStance)
+            {
+                print("Go to stire stance");
+                _timestampStireBlock = Time.time;
+                _isInStireStance = true;
+            }
+
+            if(ingredientName == "shake" && _actualPositionIndex == 0 && !_isInShakeStance)
+            {
+                print("Go to shake stance");
+                _timestampShakeBlock = Time.time;
+                _isInShakeStance = true;
+            }
         }
 
-        // TODO : detect shake movement, and add null ingredient if it's not done well
-        if (Input.GetButtonDown("DPad_XAxis_1") && _actualPositionIndex == 0)
+        // Check if we're the first to be in shake stance
+        if (_timestampShakeBlock >= otherPlayer.GetComponent<PlayerMovement>()._timestampShakeBlock && _isInShakeStance)
         {
-            // AddIngredient();
+            _canMoveHigh = false;
+            _canMove = false;
+
+            // Detect shake movement, the player must go between 0.5 and -0.5 between each movement
+            if (Input.GetAxisRaw("DPad_YAxis_1") > 0.5 && _detectShakeValue)
+            {
+                _actualShakeCombination += "U";
+                _detectShakeValue = false;
+            }
+            else if (Input.GetAxisRaw("DPad_YAxis_1") < -0.5 && _detectShakeValue)
+            {
+                _actualShakeCombination += "D";
+                _detectShakeValue = false;
+            }
+            else if (!_detectShakeValue && Input.GetAxisRaw("DPad_YAxis_1") > -0.5 && Input.GetAxisRaw("DPad_YAxis_1") < 0.5)
+            {
+                _detectShakeValue = true;
+            }
+            
+            // Check if a shake movement has been detected
+            if (_shakeCombination.StartsWith(_actualShakeCombination))
+            {
+                if (_shakeCombination == _actualShakeCombination)
+                {
+                    // Shake success
+                    print("Shake is okay !");
+                    SuccessShake(false);
+                    _actualShakeCombination = "";
+                    _isInShakeStance = false;
+                }
+            }
+            else
+            {
+                // Not good
+                print("Shake fail");
+                FailMove();
+                _actualShakeCombination = "";
+                _isInShakeStance = false;
+            }
+        }
+
+        // Check if we're the first to be in stire stance
+        if (_timestampStireBlock >= otherPlayer.GetComponent<PlayerMovement>()._timestampStireBlock && _isInStireStance)
+        {
+            _canMoveHigh = false;
+            _canMove = false;
+
+            // Detect stire inputs
+            if (Input.GetButtonDown("A_1") )
+            {
+                _actualStireCombination += "A";
+            }
+            else if (Input.GetButtonDown("B_1"))
+            {
+                _actualStireCombination += "B";
+            }
+            else if (Input.GetButtonDown("Y_1"))
+            {
+                _actualStireCombination += "Y";
+            }
+            else if (Input.GetButtonDown("X_1"))
+            {
+                _actualStireCombination += "X";
+            }
+
+            // Check if a stire movement has been detected
+            if (_stireCombination.StartsWith(_actualStireCombination))
+            {
+                if(_stireCombination == _actualStireCombination)
+                {
+                    // Stire success
+                    print("Stire is okay !");
+                    SuccessStire(false);
+                    _actualStireCombination = "";
+                    _isInStireStance = false;
+                }
+            }
+            else if (_stireReverseCombination.StartsWith(_actualStireCombination))
+            {
+                if (_stireReverseCombination == _actualStireCombination)
+                {
+                    // Reverse stire success
+                    print("Reverse stire is okay !");
+                    SuccessStire(true);
+                    _actualStireCombination = "";
+                    _isInStireStance = false;
+                }
+            }
+            else
+            {
+                // Not good
+                print("Stire fail");
+                FailMove();
+                _actualStireCombination = "";
+                _isInStireStance = false;
+            }
         }
 
         // TODO : detect light it up, and add null ingredient if it's not done well
@@ -80,6 +220,28 @@ public class PlayerMovement : MonoBehaviour {
             print("LIGHT IT UP");
             // AddIngredient();
         }
+    }
+
+    private void SuccessStire(bool reverse)
+    {
+        if(reverse)
+        {
+            LevelManager.Manager.AddIngredient(_playerId, StireReverse);
+        }
+        else
+        {
+            LevelManager.Manager.AddIngredient(_playerId, Stire);
+        }
+    }
+
+    private void SuccessShake(bool reverse)
+    {
+        LevelManager.Manager.AddIngredient(_playerId, Shake);
+    }
+
+    private void FailMove()
+    {
+        LevelManager.Manager.AddIngredient(_playerId, null);
     }
 
     private void AddIngredient()
