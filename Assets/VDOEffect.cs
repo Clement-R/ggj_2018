4daff5419801eb4d0d39688bb395926812572b5e
Clement-R <rondeau.clement@gmail.com>
2018-01-28 16:21:06 +0100

    add VDO beat effect

--- Assets/VDOEffect.cs
+++ Assets/VDOEffect.cs
@@ -24,7 +24,7 @@ public class VDOEffect : MonoBehaviour {
     IEnumerator Effect()
     {
         _sr.sprite = flash;
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.25f);
         _sr.sprite = idle;
     }
 }
