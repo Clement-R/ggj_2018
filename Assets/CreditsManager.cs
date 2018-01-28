df2125d1b05e8b2036014189a4c6f3f072c601a3
Clement-R <rondeau.clement@gmail.com>
2018-01-28 16:56:36 +0100

    Final commit

---
+++ Assets/CreditsManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsManager : MonoBehaviour {
+
+    public float alphaScan = 0.3f;
+
+    private void Start()
+    {
+        Scanline scanline = Camera.main.GetComponent<Scanline>();
+        if (scanline)
+        {
+            scanline.SetAlpha(alphaScan);
+        }
+    }
+
+    void Update ()
+    {
+		if(Input.GetButtonDown("A_1") || Input.GetButtonDown("X_1") || Input.GetButtonDown("B_1") || Input.GetButtonDown("Y_1"))
+        {
+            ScreenEffectsManager.SwitchToScene("main_menu");
+        }
+	}
+}
